2e42b1ca3d21923d389f2d0fb8fecfd687e2afff
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-03-28 11:46:13 -0300

    pilhaEstatica

--- src/Pilhas/Main.java
+++ src/Pilhas/Main.java
@@ -2,10 +2,16 @@ package Pilhas;
 
 public class Main {
     public static void main(String[] args) {
-        PilhaEstatica<Integer> stack = new PilhaEstatica<>();
+        PilhaDinamica<Integer> stack = new PilhaDinamica<>();
 
         stack.push(1);
         stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.pop();
+        System.out.println(stack.peek());
         System.out.println(stack);
     }
 }
--- src/Pilhas/PilhaDinamica.java
+++ src/Pilhas/PilhaDinamica.java
@@ -4,7 +4,51 @@ public class PilhaDinamica<T> {
     private No<T> topo;
 
     public void push(T elemento) {
+        No<T> novo = new No<>(elemento);
 
+        if(this.topo == null) {
+            this.topo = novo;
+        } else {
+            No<T> aux = this.topo;
+            while (aux.proximo!=null) {
+                aux = aux.proximo;
+            }
+            aux.proximo=novo;
+        }
+    }
+
+    public void pop() {
+        if(this.topo == null) {
+            this.topo = null;
+        } else {
+            No<T> aux = this.topo;
+            while (aux.proximo.proximo!=null) {
+                aux = aux.proximo;
+            }
+            aux.proximo=null;
+        }
+    }
+
+    public T peek() {
+        No<T> aux = this.topo;
+        while (aux.proximo!=null) {
+            aux = aux.proximo;
+        }
+        return aux.dado;
+    }
+
+    @Override
+    public String toString() {
+        String print = "[";
+
+        No<T> aux = this.topo;
+        while (aux.proximo!=null) {
+            print+= aux.dado+", ";
+            aux = aux.proximo;
+        }
+        print+= aux.dado+"]";
+
+        return print;
     }
 
 }
