a984ce95fc247103599063f9dc408ecf02c67332
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-04-19 15:55:24 -0300

    InfixaPraPosfixa

--- src/InfixaPraPosfixa/Main.java
+++ src/InfixaPraPosfixa/Main.java
@@ -1,6 +1,6 @@
-/*package InfixaPraPosfixa;
+package InfixaPraPosfixa;
 
-import Pilhas.PilhaDinamicaPrintJunto;
+import Pilhas.PilhaDinamica;
 
 import java.util.Scanner;
 
@@ -9,65 +9,59 @@ import static java.lang.Character.isLetter;
 public class Main {
     public static void main(String[] args) {
         Scanner dado = new Scanner(System.in);
+        int qtdCasos;
+        char ch;
+        String expressao;
+        String resultado;
+        PilhaDinamica<Character> operadores;
 
-
-        int qtdCasos = dado.nextInt();
+        qtdCasos = dado.nextInt();
 
         for(int i = 0; i < qtdCasos; i++) {
-            PilhaDinamicaPrintJunto<Character> resultado = new PilhaDinamicaPrintJunto<>();
-            PilhaDinamicaPrintJunto<Character> operadores = new PilhaDinamicaPrintJunto<>();
-            String expressao = dado.next();
+            operadores = new PilhaDinamica<>();
+            expressao = dado.next();
+            resultado = "";
 
             for(int j = 0; j < expressao.length(); j++) {
-                char ch = expressao.charAt(j);
+                ch = expressao.charAt(j);
 
-                if (!isLetter(ch) && Character.isDigit(ch)) {
-                    while (!resultado.empty() && hasHigherPrecedence(ch, resultado.peek())) {
-
-                        char connective = resultado.peek();
-                        resultado.pop();
-
-                        resultado.push();
-                        boolean operand2 = operands.pop();
-                        boolean operand1 = operands.pop();
-                        boolean result = applyConnective(connective, operand1, operand2);
-
-                        operands.push(result);
+                if(isLetter(ch) || Character.isDigit(ch)) {
+                    resultado+=ch;
+                } else if(ch == '(') {
+                    operadores.push(ch);
+                } else if(ch == ')') {
+                    while (operadores.peek() != '(') {
+                        resultado+=operadores.peek();
+                        operadores.pop();
                     }
-                    resultado.push(ch);
+                    operadores.pop();
+                } else {
+                    while(!operadores.empty() && !(operadores.peek()=='(') && (precedencia(ch) <= precedencia(operadores.peek()))) {
+                        resultado+=operadores.peek();
+                        operadores.pop();
+                    }
+                    operadores.push(ch);
                 }
             }
 
-        }
-    }
-
-    public static boolean hasHigherPrecedence(char connective1, char connective2) {
-        int priority1 = 0;
-        int priority2 = 0;
-
-        //Quanto menor o número, mais prioridade ele tem
-        for(char ch = connective1, i = 0;  i<2 ;ch = connective2, i++) {
-            switch (ch) {
-                case '*':
-                    if (i == 0) priority1 = 1;
-                    else priority2 = 1;
-                case '/':
-                    if (i == 0) priority1 = 1;
-                    else priority2 = 1;
-                case '-':
-                    if (i == 0) priority1 = 3;
-                    else priority2 = 3;
-                case '+':
-                    if (i == 0) priority1 = 3;
-                    else priority2 = 3;
+            while(!operadores.empty()) {
+                resultado+=operadores.peek();
+                operadores.pop();
             }
-        }
 
-        if(priority1>priority2) {
-            return true;
-        }
-        else {
-            return false;
+            System.out.println(resultado);
         }
     }
-}*/
+
+    static int precedencia(char operador) {
+        if (operador == '+' || operador == '-') {
+            return 1;
+        } else if (operador == '*' || operador == '/') {
+            return 2;
+        } else if (operador == '^') {
+            return 3;
+        } else {
+            return 0;
+        }
+    }
+}
